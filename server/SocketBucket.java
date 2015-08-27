0b96ae58a7b310536aae253a5c08c626498db191
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2015-08-27 13:00:10 +0900

    edit

---
+++ server/SocketBucket.java
@@ -0,0 +1,15 @@
+
+
+
+
+class SocketBucket{
+	private Map<String, Socket> sockets;
+
+	public void putSockets(String username, Socket socket){
+
+	}
+
+	public void getSockets(){
+		return sockets;
+	}
+}
